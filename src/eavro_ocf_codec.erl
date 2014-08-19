077c9cd589f2b64b11c750c428cc02752e532306
Vyacheslav Vorobyov <vjache@gmail.com>
2014-08-19 15:32:20 +0400

    When decode OCF return also schema(extracted from header) along with data set parsed. Add spec.

--- src/eavro_ocf_codec.erl
+++ src/eavro_ocf_codec.erl
@@ -1,17 +1,23 @@
 -module(eavro_ocf_codec).
 
+-include("eavro.hrl").
+
 -export([decode/2]).
 
 %% See: http://avro.apache.org/docs/1.7.7/spec.html#Object+Container+Files
 
+-spec decode(Bin :: binary(), 
+	     Hook :: decode_hook() ) -> 
+		    {Schema :: avro_type(), 
+		     Blocks :: [ [ any() ] ]}.
 decode(Bin, Hook) ->
     PrivDir = code:priv_dir(eavro),
     OcfSchema = eavro:read_schema(filename:join(PrivDir, "avro_ocf.avsc")),
     {_Header = [_,Meta,_], Bin1} = eavro_codec:decode(OcfSchema,Bin),
     SchemaJson = proplists:get_value(<<"avro.schema">>, lists:flatten(Meta)),
-    Codec = proplists:get_value(<<"avro.codec">>, lists:flatten(Meta)),
+    Codec  = proplists:get_value(<<"avro.codec">>, lists:flatten(Meta)),
     Schema = eavro:parse_schema(SchemaJson),
-    decode_blocks(Schema, Bin1, Codec, Hook).
+    {Schema, decode_blocks(Schema, Bin1, Codec, Hook)}.
 
 decode_blocks(_Schema, <<>>, Codec, _Hook) -> [];
 decode_blocks(Schema, Buff, Codec, Hook) ->
