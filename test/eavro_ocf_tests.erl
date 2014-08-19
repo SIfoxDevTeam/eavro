14e4d383b4ce567297c61ef0a8a7cdd318aab287
Vyacheslav Vorobyov <vjache@gmail.com>
2014-08-19 15:35:04 +0400

    Add OCF test for dataset generated with Java Avro tools.

--- test/eavro_ocf_tests.erl
+++ test/eavro_ocf_tests.erl
@@ -7,28 +7,42 @@
 parse_twitter_ocf_test() ->
     Data = eavro:read_ocf("../test/data/twitter.avro"),
     ?assertMatch( 
-       _Blocks = 
+       {Schema = #avro_record{},
+	_Blocks = 
 	   [_Block = 
 		[ _Rec1 = [<<"miguno">>,<<"Rock: Nerf paper, scissors is fine.">>,
 			   1366150681],
 		  _Rec2 = [<<"BlizzardCS">>,<<"Works as intended.  Terran is IMBA.">>,
 			   1366154481]
 		]
-	   ], Data).
+	   ]}, Data).
 
 parse_twitter_ocf_with_hook_test() ->
-    Data = lists:flatten(
-	     eavro:read_ocf("../test/data/twitter.avro", fun simple_hook/2)),
+    {Schema = #avro_record{}, 
+     Data } = eavro:read_ocf("../test/data/twitter.avro", fun simple_hook/2),
     ?assertMatch(
-       [{<<"miguno">>,<<"Rock: Nerf paper, scissors is fine.">>,
-	 1366150681},
-	{<<"BlizzardCS">>,<<"Works as intended.  Terran is IMBA.">>,
-	 1366154481} ], 
-       Data).
+	[{"miguno","Rock: Nerf paper, scissors is fine.",1366150681},
+	 {"BlizzardCS","Works as intended.  Terran is IMBA.",1366154481} ], 
+       lists:flatten(Data)).
+
+parse_transformers_ocf_test() ->
+    Data = eavro:read_ocf("../test/data/transformers.avro"),
+    ?assertMatch( 
+       {Schema = #avro_record{},
+	_Blocks = 
+	   [_Block = 
+		[ _Rec1 = [<<"Optimus">>,<<"Prime">> |_] | _ ]
+	   ]}, Data).
+
+
+%%
+%% Private functions
+%%
 
 simple_hook(#avro_record{name='twitter_schema'}, 
 	    [Name, Tweet, Timestamp]) ->
     {Name, Tweet, Timestamp};
+simple_hook(string, V) -> binary_to_list(V);
 simple_hook(_, V) -> V.
 
     
