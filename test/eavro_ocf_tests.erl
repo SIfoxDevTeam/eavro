f85c6cb587cd33bc16175054d289b2d8da9f7e78
Vyacheslav Vorobyov <vjache@gmail.com>
2014-08-27 15:09:31 +0400

    Add tests for writing OCFs (deflated, multiblocked etc).

--- test/eavro_ocf_tests.erl
+++ test/eavro_ocf_tests.erl
@@ -47,6 +47,55 @@ parse_transformers_ocf_deflated_test() ->
 	    _Instance = [<<"0000">>, <<"Optimus">>,<<"Prime">> |_] | _ 
 	   ]}, Data).
 
+ocf_read_write_read_test_() ->
+    Test = 
+	fun(F1, F2, Opts) ->
+		file:delete(F2),
+		?assert(not filelib:is_regular(F2)),
+		ok = eavro_ocf_zcodec:read_ocf_with(
+		  F1,
+		  fun(Schema, ZInstances) ->
+			  eavro_ocf_zcodec:write_ocf_file(
+			    F2,
+			    Schema, ZInstances, Opts)
+		  end, 
+		  fun eavro_util:std_hook/2 ),
+		Cnt = eavro_ocf_zcodec:read_ocf_with(
+		  F2, 
+		  fun(Schema, ZInstances) ->
+			  zlists:count(ZInstances)
+		  end),
+		?assert( Cnt > 0 )
+	end,
+    {inorder, 
+     [{Title,
+      fun() -> Test(F1,F2,Opts) end} || 
+	 {Title, F1, F2, Opts} <- 
+	     [{"Read plain, write plain, read result.",
+	       "../test/data/transformers.avro",
+	       "transformers.avro",
+	       []},
+	      {"Read plain, write plain multiblocked, read multiblocked result.",
+	       "../test/data/transformers.avro",
+	       "transformers-mult.avro",
+	       [{block_size, 128}]},
+	      {"Read deflated, write plain, read result.",
+	       "../test/data/transformers-deflated.avro",
+	       "transformers-infl.avro",
+	       []},
+	      {"Read deflated, write deflated, read deflated result.",
+	       "../test/data/transformers-deflated.avro",
+	       "transformers-deflated.avro",
+	       [{codec, deflated}]},
+	      {"Read deflated multiblocked, write deflated, read deflated result.",
+	       "../test/data/transformers-deflated2.avro",
+	       "transformers-deflated2.avro",
+	       [{codec, deflated}]},
+	      {"Read deflated multiblocked, write deflated mult, read deflated result.",
+	       "../test/data/transformers-deflated2.avro",
+	       "transformers-deflated2m.avro",
+	       [{codec, deflated},{block_size, 128}]}
+	     ] ]}.
 %%
 %% Private functions
 %%
